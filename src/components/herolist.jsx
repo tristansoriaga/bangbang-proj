ceeaeacbfd0d32eb8bd496ee959d06faf390a688
tristansoriaga <35946776+tristansoriaga@users.noreply.github.com>
2018-10-29 19:26:38 -0700

    Added a flip move animation on filter

--- src/components/herolist.jsx
+++ src/components/herolist.jsx
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
 import "./herolist.css";
+import FlipMove from "react-flip-move";
 
 class Herolist extends Component {
   render() {
     return (
-      <div>
-        <div className="herolist">
-          {this.props.heroes.map(val => (
-            <div className="hero-box" key={val.id}>
-              <div className="hero-image">{val.name}</div>
-              <div className="hero-caption">{val.type}</div>
-            </div>
-          ))}
-        </div>
-      </div>
+      <FlipMove className="herolist">
+        {this.props.heroes.map(val => (
+          <div className="hero-box" key={val.id}>
+            <div className="hero-image">{val.name}</div>
+            <div className="hero-caption">{val.type}</div>
+          </div>
+        ))}
+      </FlipMove>
     );
   }
 }
