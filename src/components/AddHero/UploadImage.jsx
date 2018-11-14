9554faab16e803a693258aea1751263568ef0f59
Tristan Soriaga <tristan_soriaga@apple.com>
2018-11-14 18:29:49 +0800

    Built initial upload component

--- src/components/AddHero/UploadImage.jsx
+++ src/components/AddHero/UploadImage.jsx
@@ -1,19 +1,65 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import CheckIcon from "@material-ui/icons/Check";
-import SaveIcon from "@material-ui/icons/Save";
+import CloudUpload from "@material-ui/icons/CloudUpload";
+import CloudDone from "@material-ui/icons/CloudDone";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import fire from "../../firebase";
 
+const DivUploadImage = styled.div`
+  width: 500px;
+  background-color: white;
+`;
 class UploadImage extends Component {
   state = {
     loading: false,
-    success: false
+    success: false,
+    name: ""
   };
 
   componentWillUnmount() {
     clearTimeout(this.timer);
   }
+
+  handleChange = e => {
+    if (e.target.files[0] !== undefined) {
+      this.setState({
+        success: false,
+        loading: true
+      });
+
+      var file = e.target.files[0];
+      var storageRef = fire.storage().ref("images123/" + file.name);
+      storageRef.put(file);
+
+      var task = storageRef.put(file);
+      task.on(
+        "state_changed",
+        snapshot => {
+          switch (snapshot.state) {
+            case "paused": // or 'paused'
+              console.log("Upload is paused");
+              break;
+            case "running": // or 'running'
+              console.log("Upload is running");
+              break;
+          }
+        },
+        error => {},
+        () => {
+          storageRef.getDownloadURL().then(url => {
+            console.log(url);
+            this.setState({
+              name: url,
+              success: true,
+              loading: false
+            });
+          });
+        }
+      );
+    }
+  };
 
   handleButtonClick = () => {
     if (!this.state.loading) {
@@ -68,35 +114,38 @@ class UploadImage extends Component {
   render() {
     const { loading, success } = this.state;
     return (
-      <div>
-        <input
-          accept="image/*"
-          id="contained-button-file"
-          multiple
-          type="file"
-          style={{ display: "none" }}
-        />
-        <label htmlFor="contained-button-file">
-          <Button
-            variant="contained"
-            component="span"
-            color="secondary"
-            onClick={this.handleButtonClick}
-          >
-            Upload
-          </Button>
-        </label>
-        <div>
-          <Button
-            variant="fab"
-            color="primary"
-            onClick={this.handleButtonClick}
-          >
-            {success ? <CheckIcon /> : <SaveIcon />}
-          </Button>
-          {loading && <CircularProgress size={68} />}
-        </div>
-      </div>
+      <React.Fragment>
+        <DivUploadImage>
+          <TextField
+            value={this.state.name}
+            name="name"
+            label="Name"
+            style={{ margin: 8, width: "350px" }}
+            placeholder="Name"
+            margin="normal"
+            variant="outlined"
+            required
+          />
+          <input
+            onChange={this.handleChange}
+            accept="image/*"
+            id="contained-button-file"
+            multiple
+            type="file"
+            style={{ display: "none" }}
+          />
+          <label htmlFor="contained-button-file">
+            <Button
+              variant="contained"
+              component="span"
+              color={success ? "secondary" : "primary"}
+            >
+              {success ? <CloudDone /> : <CloudUpload />}
+            </Button>
+            {loading && <CircularProgress size={30} />}
+          </label>
+        </DivUploadImage>
+      </React.Fragment>
     );
   }
 }
